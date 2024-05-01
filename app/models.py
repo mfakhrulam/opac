9f5ec3ebf2e3d09723252465bc11e57e5bddadee
Muhammad Fakhrul Amin <80448871+mfakhrulam@users.noreply.github.com>
2024-05-01 17:24:29 +0700

    Change the db model and migration

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
+  __tablename__ = 'users'
   id: so.Mapped[int] = so.mapped_column(primary_key=True)
   username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
   password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
@@ -20,17 +21,38 @@ class User(db.Model):
   def __repr__(self):
     return '<User {}>'.format(self.username)
   
-class Docs(db.Model):
+class Doc(db.Model):
+  __tablename__ = 'docs'
   id: so.Mapped[int] = so.mapped_column(primary_key=True)
   title: so.Mapped[str] = so.mapped_column(sa.String(255), index=True)
   year: so.Mapped[int] = so.mapped_column()
   author: so.Mapped[str] = so.mapped_column(sa.String(255))
   classification: so.Mapped[str] = so.mapped_column(sa.String(255))
+  subject: so.Mapped[str] = so.mapped_column(sa.String(255))
   publisher: so.Mapped[str] = so.mapped_column(sa.String(255))
   abstract: so.Mapped[str] = so.mapped_column(sa.Text)
   location: so.Mapped[str] = so.mapped_column(sa.String(255))
-  created_at: so.Mapped[datetime] = so.mapped_column(insert_default=sa.func.now())
-  updated_at: so.Mapped[Optional[datetime]] = so.mapped_column(nullable=True)
+  created_at: so.Mapped[datetime] = so.mapped_column(insert_default=sa.func.now(), server_default=sa.text('CURRENT_TIMESTAMP'))
+  updated_at: so.Mapped[Optional[datetime]] = so.mapped_column(nullable=True, onupdate=sa.func.now())
+  
+  def get_paginated_docs(page, per_page=20):
+    page = db.paginate(db.select(Doc).order_by(Doc.created_at), page=page, per_page=per_page)
+    return page
+  
+  def to_json(self):
+    return {
+      'id': self.id,
+      'title': self.title,
+      'year': self.year,
+      'author': self.author,
+      'classification': self.classification,
+      'subject': self.subject,
+      'publisher': self.publisher,
+      'abstract': self.abstract,
+      'location': self.location,
+      'created_at': self.created_at,
+      'updated_at': self.updated_at
+    }
   
   def __repr__(self):
     return '<Docs {}>'.format(self.title)
