73112a76e4db1c70e026d645a334b8f6c77cfa68
Muhammad Fakhrul Amin <80448871+mfakhrulam@users.noreply.github.com>
2024-05-01 17:23:09 +0700

    Update tailwind.config.js and package json

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,16 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    "./templates/**/*.html",
-    "./static/src/**/*.js"
+    "./app/templates/**/*.html",
+    "./app/static/src/**/*.js",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+      }
+    },
   },
   plugins: [
     require("flowbite/plugin")
