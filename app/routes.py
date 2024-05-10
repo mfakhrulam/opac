bb8083feed29aada9af37626704cb77a0f209d00
Muhammad Fakhrul Amin <80448871+mfakhrulam@users.noreply.github.com>
2024-05-10 22:07:49 +0700

    Add QE page, favicon, and logo

--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,8 @@ from app.api_utils import read_index
 from app.models import Doc
 from flask import redirect, render_template, request, url_for
 from helper import preprocessing, query_expansion
-from process_docs import get_list_docs, load_vectorizer_sparse
 from sklearn.metrics.pairwise import cosine_similarity
-import pandas as pd
 from PaginatedObj import PaginateObj
-from sqlalchemy import func
 
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
@@ -38,28 +35,29 @@ def search():
   # TODO
   # Implementasikan jumlah kata untuk QE
   
+  nqe = request.args.get("nqe") or 1
   query = request.args.get("query").strip()
   curr_page = request.args.get("page") or 1
   if not query:
     return redirect(url_for('index')) 
-  
-  print('query awal', query)
     
-  tf_idf_vectorizer, tf_idf_sparse, idx_to_docid = read_index()
+  tf_idf_vectorizer, tf_idf_sparse = read_index()
   docs = Doc.query.all() # return dalam bentuk list
   
   # logic preprocessing dan QE
   query = preprocessing(query)
   sastrawi_stemmer = StemmerFactory().create_stemmer()
   stop = StopWordRemoverFactory().get_stop_words()
-  query = " ".join([word for word in query.split() if word not in stop])
-  if request.args.get('qe') == 'w2v':
-    vocab = tf_idf_vectorizer.get_feature_names_out()
-    query = query_expansion(w2v_model, query, vocab, topn=3)
-    query = sastrawi_stemmer.stem(query)
-  # TODO
+  # stopword (kayaknya perlu dihapus)
+  # query = " ".join([word for word in query.split() if word not in stop])
   # cek lebih baik stem dulu atau stem belakangan (di sini)
   query = sastrawi_stemmer.stem(query)
+  print('query awal: ', query)
+  
+  if request.args.get('qe') == 'w2v':
+    vocab = tf_idf_vectorizer.get_feature_names_out()
+    query = query_expansion(w2v_model, query, vocab, topn=int(nqe))
+    query = sastrawi_stemmer.stem(query)  
   
   # transform ke vector
   query_vector = tf_idf_vectorizer.transform([query])
@@ -79,7 +77,7 @@ def search():
   paginated_results = query_results.paginate(page=int(curr_page), per_page=10, error_out=False)
   zip_results = zip(paginated_results, paginated_docs_similarities_score.items)
 
-  print('query akhir', query)
+  print('query akhir:', query)
   return render_template('index.html', page=paginated_results, total_docs=total_docs, zip_results=zip_results)
 
 
@@ -87,6 +85,17 @@ def search():
 def docs(id):
   return render_template("docs.html", id=id)
 
+@app.route("/qe", methods=["GET", "POST"])
+def qe():
+  if not request.args.get('query'):
+    return render_template("qe.html", result='')
+  query = request.args.get("query")
+  query = preprocessing(query)
+  nqe = request.args.get("nqe") or 1
+  tf_idf_vectorizer, _= read_index()
+  vocab = tf_idf_vectorizer.get_feature_names_out()
+  query = query_expansion(w2v_model, query, vocab, topn=int(nqe))
+  return render_template("qe.html", result=query)
 
 # ======== ADMIN ========
 @app.route("/admin/login")
