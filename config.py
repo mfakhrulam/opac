2e7e993d36e7a4959f0d8006349741f98da515bb
Muhammad Fakhrul Amin <80448871+mfakhrulam@users.noreply.github.com>
2024-05-16 11:53:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+  SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-very-secret-key'
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:@localhost/db_opac' 
   UPLOAD_FOLDER = 'app/static/uploads'
   W2V_MODEL = 'we_model\idwiki_new_lower_word2vec_200\idwiki_new_lower_word2vec_200.model'
-  # SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+  # SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-very-secret-key'
