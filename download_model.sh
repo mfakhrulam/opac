d1dc1e4bc400cc078982cd3dcf63807e5d2165a7
Muhammad Fakhrul Amin <80448871+mfakhrulam@users.noreply.github.com>
2024-07-24 19:53:31 +0700

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Ganti dengan URL file Google Drive Anda
-# FILE_ID="1VyBV3E7C7VlW9RtqNQEnBe0CVs7wiRsY" model
-FILE_ID="1hkEyOA2gl4iPeXs5a4-hsk01CgBDXPYV"
+FILE_ID="1VyBV3E7C7VlW9RtqNQEnBe0CVs7wiRsY" 
+# FILE_ID="1hkEyOA2gl4iPeXs5a4-hsk01CgBDXPYV"
 DESTINATION="./we_model/idwiki_new_lower_word2vec_300.zip"
 
 # Download file dari Google Drive
